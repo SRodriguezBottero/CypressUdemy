e9a906c2c49ba63c5a83f7465ef21040f3de0eff
Sebastian Endava <sebastiandario.rodriguez@endava.com>
2023-08-01 10:49:29 -0300

    Initial commit

---
+++ cypress/integration/3-FreeRangeTesteres/HomePage.spec.js
@@ -0,0 +1,9 @@
+describe('Home de www.freerangetesters.com', () => {
+    beforeEach(() => {
+        cy.visit('https://www.freerangetesters.com/')
+    })
+
+    it('should have a title', () => {
+        cy.title().should('include', 'Free Range Testers')
+    })
+})
