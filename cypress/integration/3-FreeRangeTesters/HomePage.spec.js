c04e830be5f3a58d74aced5db57c06cee584066e
SRodriguezBottero <56900882+SRodriguezBottero@users.noreply.github.com>
2024-09-05 14:37:27 -0300

    Updates to the code

--- cypress/integration/3-FreeRangeTesters/HomePage.spec.js
+++ cypress/integration/3-FreeRangeTesters/HomePage.spec.js
@@ -5,5 +5,7 @@ describe('Home de www.freerangetesters.com', () => {
 
     it('should have a title', () => {
         cy.title().should('include', 'Free Range Testers')
+        cy.xpath('//*[@id="comp-l02x1m8d1label"]').click()
+        cy.contains('Iniciar Sesión')
     })
 })
